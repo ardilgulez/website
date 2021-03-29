8f5a40bd0bcc16bcadb6e728a7dc066fcac1dc82
Kemal Ardıl Gülez <ardilgulez@gmail.com>
2021-03-29 09:04:49 +0200

    Responsive Design #1

---
+++ components/header/header.js
@@ -0,0 +1,39 @@
+import * as styles from './header.module.scss';
+import Link from 'next/link';
+
+export default function Header({ activeTab }) {
+	return (
+		<header role="banner" className={styles.header}>
+			<div className={styles.navContainer}>
+				<div className={styles.welcome}>
+					<ul className={styles.navList}>
+						<li className={styles.navListItem}>
+							<Link href="/">
+								<a className={styles.welcomeText}>Kemal Ardıl Gülez</a>
+							</Link>
+						</li>
+					</ul>
+				</div>
+				<div className={styles.nav}>
+					<ul className={styles.navList}>
+						<li key="about" className={styles.navListItem}>
+							<Link href="/">
+								<a className={activeTab === 'about' ? styles.active : styles.navListLink}>Me</a>
+							</Link>
+						</li>
+						<li key="blog" className={styles.navListItem}>
+							<Link href="/blog">
+								<a className={activeTab === 'blog' ? styles.active : styles.navListLink}>Blog</a>
+							</Link>
+						</li>
+						<li key="contacts" className={styles.navListItem}>
+							<Link href="/contacts">
+								<a className={activeTab === 'contacts' ? styles.active : styles.navListLink}>Hello</a>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</header>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,54 @@
-import { Sidebar } from '../components/index';
 import * as styles from './index.module.scss';
+import { Sidebar, Footer, Header } from '../components/index';
 
 export default function About({ config }) {
 	return (
 		<div className={styles.outerContainer}>
 			<Sidebar activeTab="about" />
-			<div className={styles.pageSection}>
-				<section title="about">
-					<h1 className={styles.aboutTitle}>How about me?</h1>
-					<p>
-						I'm a full stack developer for the past 7+ years (since 2013, to be exact), now working at Hexad
-						GmbH.
-					</p>
-					<p>Currently Learning: Android App Development (Native)</p>
-					<h1 className={styles.aboutTitle} style={{ marginBottom: '2rem' }}>
-						My Employment History
-					</h1>
-					{config.employmentHistory.map((item) => (
-						<div key={item.name} className={styles.employment}>
-							<div className={styles.employmentTitle}>
-								<h2 className={styles.titleText}>{item.name}</h2>
-								<span className={styles.dateSpan}>{item.dates}</span>
-							</div>
-							<div className={styles.employmentDetails}>
-								{item.titles.map((titleItem) => (
-									<div key={titleItem.title} className={styles.employmentSubtitle}>
-										<h4 className={styles.titleText}>{titleItem.title}</h4>
-										{item.titles.length > 1 && (
-											<span className={styles.dateSpan}>{titleItem.date}</span>
-										)}
-									</div>
-								))}
-								<p className={styles.jobDetails}>{item.description}</p>
-								<ul className={[ styles.responsibilityList, styles.jobDetails ].join(' ')}>
-									{item.responsibilities.map((responsibility) => (
-										<li key={responsibility}>{responsibility}</li>
-									))}
-								</ul>
-								<div className={styles.techTagContainer}>
-									{item.technologies.map((technology) => (
-										<span key={technology} className={styles.techTag}>
-											{technology}
-										</span>
-									))}
+			<Header activeTab="about" />
+			<section className={styles.pageSection} title="about">
+				<h1 className={styles.aboutTitle}>How about me?</h1>
+				<p>
+					I'm a full stack developer for the past 7+ years (since 2013, to be exact), now working at Hexad
+					GmbH.
+				</p>
+				<p>Currently Learning: Android App Development (Native)</p>
+				<h1 className={styles.aboutTitle} style={{ marginBottom: '2rem' }}>
+					My Employment History
+				</h1>
+				{config.employmentHistory.map((item) => (
+					<div key={item.name} className={styles.employment}>
+						<div className={styles.employmentTitle}>
+							<h2 className={styles.titleText}>{item.name}</h2>
+							<span className={styles.dateSpan}>{item.dates}</span>
+						</div>
+						<div className={styles.employmentDetails}>
+							{item.titles.map((titleItem) => (
+								<div key={titleItem.title} className={styles.employmentSubtitle}>
+									<h4 className={styles.titleText}>{titleItem.title}</h4>
+									{item.titles.length > 1 && (
+										<span className={styles.dateSpan}>{titleItem.date}</span>
+									)}
 								</div>
+							))}
+							<p className={styles.jobDetails}>{item.description}</p>
+							<ul className={[ styles.responsibilityList, styles.jobDetails ].join(' ')}>
+								{item.responsibilities.map((responsibility) => (
+									<li key={responsibility}>{responsibility}</li>
+								))}
+							</ul>
+							<div className={styles.techTagContainer}>
+								{item.technologies.map((technology) => (
+									<span key={technology} className={styles.techTag}>
+										{technology}
+									</span>
+								))}
 							</div>
 						</div>
-					))}
-				</section>
-			</div>
+					</div>
+				))}
+			</section>
+			<Footer config={config} />
 		</div>
 	);
 }
