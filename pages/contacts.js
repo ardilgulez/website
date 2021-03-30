5b010737adf372058e9e2a396d5e84022650bc3f
Kemal Ardıl Gülez <ardilgulez@gmail.com>
2021-03-30 07:46:37 +0200

    Refactoring

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,4 +1,4 @@
-import { Sidebar, Footer, Header } from '../components/index';
+import { Layout } from '../components/index';
 
 import * as styles from './contacts.module.scss';
 
@@ -25,69 +25,64 @@ export default function Contacts({ config }) {
 	};
 
 	return (
-		<div className={styles.outerContainer}>
-			<Sidebar activeTab="contacts" />
-			<Header activeTab="contacts" />
-			<div className={styles.pageContainer}>
-				<div className={styles.formContainer}>
-					<h1>Say What's Up!</h1>
-					<form className={styles.contactForm} onSubmit={sendContact} autoComplete="on">
-						<div className={styles.formField}>
-							<input
-								placeholder="First Name (min 3 characters)"
-								className={[ styles.formInput, styles.formTextField ].join(' ')}
-								type="text"
-								id="firstName"
-								name="firstName"
-								autoComplete="on"
-								required={true}
-								minLength={3}
-							/>
-						</div>
-						<div className={styles.formField}>
-							<input
-								placeholder="Last Name (min 3 characters)"
-								className={[ styles.formInput, styles.formTextField ].join(' ')}
-								type="text"
-								id="lastName"
-								name="lastName"
-								autoComplete="on"
-								required={true}
-								minLength={3}
-							/>
-						</div>
-						<div className={styles.formField}>
-							<input
-								placeholder="Email (mandatory)"
-								className={[ styles.formInput, styles.formTextField ].join(' ')}
-								type="email"
-								id="email"
-								name="email"
-								required={true}
-							/>
-						</div>
-						<div className={styles.formField}>
-							<textarea
-								className={[ styles.formInput, styles.formTextArea ].join(' ')}
-								placeholder="Comments (min 50 characters)"
-								id="message"
-								name="message"
-								rows="2"
-								cols="10"
-								autoComplete="off"
-								required={true}
-								minLength={50}
-							/>
-						</div>
-						<div className={styles.formButton}>
-							<button className={styles.submitButton} type="submit" id="formSubmit">
-								Submit
-							</button>
-						</div>
-					</form>
-				</div>
+		<Layout activeTab={'contacts'} config={config}>
+			<div className={styles.formContainer}>
+				<h1>Say What's Up!</h1>
+				<form className={styles.contactForm} onSubmit={sendContact} autoComplete="on">
+					<div className={styles.formField}>
+						<input
+							placeholder="First Name (min 3 characters)"
+							className={[ styles.formInput, styles.formTextField ].join(' ')}
+							type="text"
+							id="firstName"
+							name="firstName"
+							autoComplete="on"
+							required={true}
+							minLength={3}
+						/>
+					</div>
+					<div className={styles.formField}>
+						<input
+							placeholder="Last Name (min 3 characters)"
+							className={[ styles.formInput, styles.formTextField ].join(' ')}
+							type="text"
+							id="lastName"
+							name="lastName"
+							autoComplete="on"
+							required={true}
+							minLength={3}
+						/>
+					</div>
+					<div className={styles.formField}>
+						<input
+							placeholder="Email (mandatory)"
+							className={[ styles.formInput, styles.formTextField ].join(' ')}
+							type="email"
+							id="email"
+							name="email"
+							required={true}
+						/>
+					</div>
+					<div className={styles.formField}>
+						<textarea
+							className={[ styles.formInput, styles.formTextArea ].join(' ')}
+							placeholder="Comments (min 50 characters)"
+							id="message"
+							name="message"
+							rows="2"
+							cols="10"
+							autoComplete="off"
+							required={true}
+							minLength={50}
+						/>
+					</div>
+					<div className={styles.formButton}>
+						<button className={styles.submitButton} type="submit" id="formSubmit">
+							Submit
+						</button>
+					</div>
+				</form>
 			</div>
-			<Footer config={config} />
-		</div>
+		</Layout>
 	);
 }
