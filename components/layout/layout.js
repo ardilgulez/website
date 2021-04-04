62c964d2b690c9adce434f2acf8969bdf4718ab1
Kemal Ardıl Gülez <ardilgulez@gmail.com>
2021-04-04 13:25:54 +0200

    SEO Commit

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,8 +1,32 @@
+import Head from 'next/head';
 import { Sidebar, Footer, Header } from '../';
 import * as styles from './layout.module.scss';
 
 const PageLayout = ({ config, activeTab, children }) => (
 	<div className={styles.outerContainer}>
+		<Head>
+			<link rel="icon" href="/favicon.ico" />
+			{/* Type & URL */}
+			<meta property="og:type" content="website" />
+			<meta property="og:url" content={config.url} />
+			{/* Title */}
+			<title>{config.siteTitle}</title>
+			<meta property="og:title" content={config.siteTitle} />
+			<meta name="twitter:title" content={config.siteTitle} />
+			{/* Description */}
+			<meta name="description" content={config.description} />
+			<meta property="og:description" content={config.description} />
+			<meta name="twitter:description" content={config.description} />
+			{/* Image */}
+			<meta name="image" content={config.image} />
+			<meta property="og:image" content={config.image} />
+			<meta name="twitter:image" content={config.image} />
+			{/* Twitter */}
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:creator" content="@ardilgulez" />
+			{/* Google Site Verification */}
+			<meta name="google-site-verification" content={config.googleSiteVerification} />
+		</Head>
 		<Sidebar activeTab={activeTab} />
 		<Header activeTab={activeTab} />
 		<div className={styles.pageContainer}>{children}</div>
